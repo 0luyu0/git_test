2cf639a9f3dbb015d153749e264f031ee6a74f7a
0luyu0 <lu_christine@163.com>
2020-12-25 10:36:06 +0800

    第一次上传

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author : luyu
+ * @description:
+ * @date : 2020/12/25 9:16
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
